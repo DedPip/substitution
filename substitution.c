d982f2c80de6d92a3a24083f0984ff43247ee617
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-09 23:15:30 +0330

    Add demo with encryption functionality

--- substitution.c
+++ substitution.c
@@ -1,7 +1,35 @@
 #include "./libs/cs50.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    
+    string original = "abc";
+    string text = "ac";
+    string key = "xyz";
+    char new_text[strlen(text) + 1];
+    int alphabet_length = 3;
+
+    for (int i = 0, length = strlen(text); i < length; i++)
+    {
+        // get the number of text[i] (for i = 0 it's 'a') in the orignal alphabet (for i = 0 it's 1), 
+        // printf("%i\n", length);
+        int address_of_letter_in_alphabet = 0;
+
+        for (int j = 0; j < alphabet_length; j++)
+        {
+            if (original[j] == text[i])
+            {
+                address_of_letter_in_alphabet = j;
+                // printf("%i\n", address_of_letter_in_alphabet);
+                new_text[i] = key[address_of_letter_in_alphabet];
+                break;
+            }
+        }
+        
+    }
+    new_text[strlen(text)] = '\0';
+
+    printf("%s\n", new_text);
+
 }
