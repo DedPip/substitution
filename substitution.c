aad08ccd219f0e3d4a34e191286c89258687a753
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-11 14:15:17 +0330

    Add tests for when user inputs invalid key

--- substitution.c
+++ substitution.c
@@ -10,44 +10,81 @@ int main(int argc, string argv[])
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    else if (strlen(argv[1]) != 26)
+    string key = argv[1];
+    printf("%c\n", key[2]);
+    if (strlen(argv[1]) != 26)
     {
         printf("Key must contain 26 characters.\n");
         return 1;
     }
-    else
-    {  
-        string input_text = get_string("plaintext:  ");
-        string input_key = argv[1];
-
-        char encrypted[strlen(input_text) + 1];
-        int aplphabet_length = 26;
-
-        for (int i = 0, length_input_text = strlen(input_text); i < length_input_text; i++)
-        {
-            int index_in_alphabet = tolower(input_text[i]) - 'a';
-
-            if (isalpha(input_text[i]))
+    
+    int key_length = strlen(key);
+    char key_lower[key_length + 1];
+    
+    for (int i = 0; i < key_length; i++)
+    {
+        if (isalpha(key[i]))
+        {   
+            if (isupper(key[i]))
             {
-                if (islower(input_text[i]))
-                {
-                    encrypted[i] = tolower(input_key[index_in_alphabet]);
-                }
-                else if (isupper(input_text[i]))
-                {
-                    encrypted[i] = toupper(input_key[index_in_alphabet]);
-                }
-            }
-            else if (input_text[i] == ' ')
-            {
-                encrypted[i] = ' ';
+                key_lower[i] = tolower(key[i]);
             }
             else 
             {
-                encrypted[i] = input_text[i];
+                key_lower[i] = key[i];
             }
         }
-        encrypted[strlen(input_text)] = '\0';
-        printf("ciphertext: %s\n", encrypted);
+        else 
+        {
+            printf("The key must only contain letters!\n");
+            return 1;
+        }    
     }
+    key_lower[key_length] = '\0';
+    printf("%s\n", key_lower);
+    
+    for (int i = 0; i < key_length; i++)
+    {
+        for (int j = i + 1; j < key_length; j++)
+        {
+            if (key_lower[i] == key_lower[j])
+            {
+                printf("The key must not contain duplicate letters!\n");
+                return 1;
+            }
+        }
+    }
+    string input_text = get_string("plaintext:  ");
+    string input_key = argv[1];
+
+    char encrypted[strlen(input_text) + 1];
+    int aplphabet_length = 26;
+
+    for (int i = 0, length_input_text = strlen(input_text); i < length_input_text; i++)
+    {
+        int index_in_alphabet = tolower(input_text[i]) - 'a';
+
+        if (isalpha(input_text[i]))
+        {
+            if (islower(input_text[i]))
+            {
+                encrypted[i] = tolower(input_key[index_in_alphabet]);
+            }
+            else if (isupper(input_text[i]))
+            {
+                encrypted[i] = toupper(input_key[index_in_alphabet]);
+            }
+        }
+        else if (input_text[i] == ' ')
+        {
+            encrypted[i] = ' ';
+        }
+        else 
+        {
+            encrypted[i] = input_text[i];
+        }
+    }
+    encrypted[strlen(input_text)] = '\0';
+    printf("ciphertext: %s\n", encrypted);
+
 }
